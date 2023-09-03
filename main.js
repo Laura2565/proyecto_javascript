ae0410312cef702f15a9fb7ab79f7f34a82cc8fc
Laura Amaral <lauraamaral42@gmail.com>
2023-09-02 23:12:46 -0300

    agrego js

--- main.js
+++ main.js
@@ -1,87 +1,162 @@
-const carrito = document.getElementById('carrito');
-const elementos1 = document.getElementById('lista-1');
-const elementos2 = document.getElementById('lista-2');
-const lista = document.querySelector('#lista-carrito tbody');
-const vaciarCarritoBtn = document.getElementById('vaciar-carrito');
+const addToCartBtn = document.querySelector('.details__button');
+let cartNotification = document.querySelector('.header__cart--notification');
+let lastValue = parseInt(cartNotification.innerText);
 
-cargarEventListeners();
-
-function cargarEventListeners(){
-    elementos1.addEventListener('click', comprarElemento);
-    elementos2.addEventListener('click', comprarElemento);
-    carrito.addEventListener('click', eliminarElemento);
-    vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
-}
-
-function comprarElemento(e) {
-    e.preventDefault();
-    if(e.target.classList.contains('agregar-carrito')){
-        const elemento = e.target.parentElement.parentElement;
-        leerDatosElemento(elemento);
-       
-    }
-   
-}
-
-function leerDatosElemento(elemento) {
-    const infoElemento = {
-        imagen: elemento.querySelector('img').src,
-        titulo: elemento.querySelector('h3').textContent,
-        precio: elemento.querySelector('.precio').textContent,
-        id: elemento.querySelector('a').getAttribute('data-id'),
-        amount: 1
-    }
-
-    console.log(elemento);
-
-
-
-    this.insertarCarrito(infoElemento);
- 
-
-}
-
-function insertarCarrito(elemento) {
-    const row = document.createElement('tr');
-    row.innerHTML = `
-   <td>
-      <img scr="${elemento.imagen}" widht=100>
-   </td>
-   <td>
-       ${elemento.titulo}
-    </td>
-    <td>
-       ${elemento.precio}
-    </td>
-    <td>
-      <a href="#" class="borrar" data-id="${elemento.id}">X</a>
-    </td>     
-       `;
-
-    lista.appendChild(row);
-
-}
-
-
-function eliminarElemento(e) {
-    e.preventDefault();
-    let elemento;
-    if (e.target.classList.contains('borrar')) {
-
-        e.target.parentElement.parentElement.remove();
-        elemento = e.target.parentElement.parentElement;
-    }
-}
-
-
-
-function vaciarCarrito() {
-    while (lista.firstChild) {
-        lista.removeChild(lista.firstChild);
-    }
-    return false;
-}
-const saveLocal = () => {
-    localStorage.setItem('productos', JSON.stringify(elemento))
+addToCartBtn.addEventListener('click', ()=>{ 
+    lastValue = lastValue + userInputNumber;
     
-       }
+    cartNotification.innerText = lastValue;
+    cartNotification.style.display = 'block';
+    drawProductInModal();
+    
+});
+
+//Mostrar el modal con el detalle del carrito
+const cartIconBtn = document.querySelector('.header__cart');
+const cartModal = document.querySelector('.cart-modal');
+// let priceModal = document.querySelector('.cart-modal__price');
+const productContainer = document.querySelector('.cart-modal__chekout-container');
+
+cartIconBtn.addEventListener('click', ()=>{
+    cartModal.classList.toggle('show');
+
+    if(lastValue === 0){
+        productContainer.innerHTML = '<p class="cart-empty">Your cart is empty</p>';
+    }else{
+        drawProductInModal();
+    }
+    
+});
+
+//Borrar el contenido del carrito
+function deleteProduct(){
+    const deleteProductBtn = document.querySelector('.cart-modal__delete');
+    deleteProductBtn.addEventListener('click', ()=>{
+        productContainer.innerHTML = '<p class="cart-empty">Your cart is empty</p>';
+        lastValue = 0;
+        cartNotification.innerText = lastValue;
+    });
+}
+
+// Cambiar imagenes cuando se presione los botones flecha.
+const imageContainer = document.querySelector('.gallery__image-container');
+const previusGalleryBtn = document.querySelector('.gallery__previus');
+const nextGalleryBtn = document.querySelector('.gallery__next');
+let imgIndex = 1;
+
+nextGalleryBtn.addEventListener('click', ()=>{
+    changeNextImage(imageContainer);
+});
+
+previusGalleryBtn.addEventListener('click', ()=>{
+    changePreviusImage(imageContainer);
+});
+
+
+//Mostrar el modal de imagenes cuando hago click en la imagen principal.
+const imagesModal = document.querySelector('.modal-gallery__background');
+const closeModalBtn = document.querySelector('.modal-gallery__close');
+
+imageContainer.addEventListener('click', ()=>{
+    if(window.innerWidth >= 1115){
+        imagesModal.style.display = 'grid';
+    }
+    
+});
+
+closeModalBtn.addEventListener('click', ()=>{
+    imagesModal.style.display = 'none';
+});
+
+//Cambiar las imagenes principales desde los thumbnails
+let thumbnails = document.querySelectorAll('.gallery__thumnail')
+thumbnails = [...thumbnails]
+
+thumbnails.forEach(thumbnail => {
+    thumbnail.addEventListener('click', event=>{
+        console.log(event.target.id)
+        imageContainer.style.backgroundImage = `url('../images/image-product-${event.target.id}.jpg')`
+    });
+});
+
+//Cambiar las imagenes principales desde los thumbnails en el MODAL
+let modalthumbnails = document.querySelectorAll('.modal-gallery__thumnail');
+const modalImageContainer = document.querySelector('.modal-gallery__image-container')
+modalthumbnails = [...modalthumbnails]
+
+modalthumbnails.forEach(modalthumbnail => {
+    modalthumbnail.addEventListener('click', event=>{
+        console.log(event.target.id.slice(-1))
+        modalImageContainer.style.backgroundImage = `url('../images/image-product-${event.target.id.slice(-1)}.jpg')`
+    });
+});
+
+// Cambiar imagen principal de modal desde flechas en el modal
+const previusModalBtn = document.querySelector('.modal-gallery__previus');
+const nextModalBtn = document.querySelector('.modal-gallery__next');
+
+nextModalBtn.addEventListener('click', ()=>{
+    changeNextImage(modalImageContainer);
+});
+
+previusModalBtn.addEventListener('click', ()=>{
+    changePreviusImage(modalImageContainer);
+});
+
+// Mostrar el navbar cuando presiono el menu de hamburgesa
+const hamburgerMenu = document.querySelector('.header__menu');
+const modalNavbar = document.querySelector('.modal-navbar__background');
+const closeModalNavbar = document.querySelector('.modal-navbar__close-icon');
+
+modalNavbar.style.display = 'none'
+
+hamburgerMenu.addEventListener('click', ()=>{
+    console.log('abrir modal');
+    modalNavbar.style.display = 'block';
+});
+
+closeModalNavbar.addEventListener('click', ()=>{
+    modalNavbar.style.display = 'none';
+});
+
+
+
+
+
+// FUNCIONES
+
+function drawProductInModal(){
+    productContainer.innerHTML = `
+        <div class="cart-modal__details-container">
+            <img class="cart-modal__image" src="./images/image-product-1-thumbnail.jpg" alt="">
+            <div>
+            <p class="cart-modal__product">Autumn Limited Edition...</p>
+            <p class="cart-modal__price">$125 x3 <span>$375.00</span> </p>
+            </div>
+            <img class="cart-modal__delete" src="./images/icon-delete.svg" alt="delete">
+        </div>
+        <button class="cart-modal__chekount" >Checkout</button>`
+    deleteProduct()
+    let priceModal = document.querySelector('.cart-modal__price');
+    priceModal.innerHTML = `$125 x${lastValue} <span>$${lastValue*125}.00</span>`;
+}
+
+function changeNextImage(imgContainer){
+    if(imgIndex === 4){
+        imgIndex = 1;
+    }else{
+        imgIndex++;
+    }
+    imgContainer.style.backgroundImage = `url('../images/image-product-${imgIndex}.jpg')`
+}
+
+function changePreviusImage(imgContainer){
+    if(imgIndex === 1){
+        imgIndex = 4;
+    }else{
+        imgIndex--;
+    }
+    imgContainer.style.backgroundImage = `url('../images/image-product-${imgIndex}.jpg')`
+}
+
+
